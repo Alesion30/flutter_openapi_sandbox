927abfbd6c7fdb829246d6341fc3b4f6bbf5952a
Yuta Ohira <alesion90@gmail.com>
2022-10-15 12:20:17 +0900

    install router

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_openapi_sandbox/ui/pages/home.dart';
+import 'package:flutter_openapi_sandbox/router.dart';
 import 'package:flutter_openapi_sandbox/ui/theme/theme.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
       theme: AppTheme.light().data,
-      home: const HomePage(),
+      routerConfig: router,
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_openapi_sandbox/ui/pages/user.dart';
+import 'package:flutter_openapi_sandbox/ui/pages/users.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final routerProvider = Provider((ref) {
+  return GoRouter(
+    initialLocation: '/users',
+    routes: [
+      GoRoute(
+        path: '/users',
+        builder: (context, state) => const UsersPage(),
+      ),
+      GoRoute(
+        path: '/user/:id',
+        builder: (context, state) {
+          final id = state.params['id']!;
+          return UserPage(id);
+        },
+      ),
+    ],
+  );
+});
