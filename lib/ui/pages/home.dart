35b381002ff4f04db1b9caf26585c8b0ecad7222
Yuta Ohira <alesion90@gmail.com>
2022-10-15 11:03:01 +0900

    call openapi

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:openapi/openapi.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -9,6 +10,15 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void getUsers() async {
+      final api = Openapi(
+        basePathOverride: 'http://localhost:8082',
+      );
+      final repository = api.getUsersApi();
+      final response = await repository.getUsers();
+      print(response.data);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -16,8 +26,11 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('You have pushed the button this many times:'),
+          children: [
+            ElevatedButton(
+              onPressed: getUsers,
+              child: const Text('fetch users'),
+            )
           ],
         ),
       ),
